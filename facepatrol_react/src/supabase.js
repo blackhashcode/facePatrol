d5b5416b474cb11dad4c86e31f9ed76276e0ef79
Arshad Uzzaman <arshaduzzaman156@live.com>
2024-04-15 01:04:43 +0600

    added keys to env

--- facepatrol_react/src/supabase.js
+++ facepatrol_react/src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qwpwaxwdkqewcjcmlrvp.supabase.co"; // You can find this in your project settings on Supabase
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3cHdheHdka3Fld2NqY21scnZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMxMTU1NDQsImV4cCI6MjAyODY5MTU0NH0.z119zIBDL-FHX1wFEllQVbIHNsmSL5gnIYRPJdcN4Hw"; // Also found in project settings
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // You can find this in your project settings on Supabase
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY; // Also found in project settings
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
